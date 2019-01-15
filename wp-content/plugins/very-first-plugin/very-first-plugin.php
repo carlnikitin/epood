973f9f146bb548f3fa3c71c0880db4965cacc560
carlnikitiniktkh <carl.nikitin@khk.ee>
2019-01-15 12:26:20 +0200

    modified plugin `Very First Plugin` version 1.0 (after activation of `Very First Plugin` version 1.0)

---
+++ wp-content/plugins/very-first-plugin/very-first-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Very First Plugin
+ * Plugin URL: http://carlnikitin.ikt.khk.ee/
+ * Description: This is the very first plugin I ever created
+ * Version: 1.0
+ * Author: Carl Nikitin
+ * Author URL: http://carlnikitin.ikt.khk.ee/
+ **/
